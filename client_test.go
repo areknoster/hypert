c4e25946197e105cd15f3898d751ab646da6724d
Arkadiusz Noster <arkadiusz.noster@gmail.com>
2024-03-12 23:00:26 +0100

    fix: fix caller dir test for windows

--- client_test.go
+++ client_test.go
@@ -5,17 +5,21 @@ import (
 	"testing"
 )
 
+import (
+	"path/filepath"
+)
+
 func Test_callerDir(t *testing.T) {
 	call := func() string {
 		// the callstack depth of the package user
 		return callerDir()
 	}
-	cd := call()
+	cd := filepath.ToSlash(call())
 	cwd, err := os.Getwd()
 	if err != nil {
 		t.Fatal(err)
 	}
-	if cd != cwd {
+	if cd != filepath.ToSlash(cwd) {
 		t.Fatalf("expected %s, got %s", cwd, cd)
 	}
 }
